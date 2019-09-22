e276628b5f7ca0e445a63bb781ca9f15785986a0
vuduland <53414263+vuduland@users.noreply.github.com>
2019-09-22 12:28:10 -0500

    added some css and bootstrap, but doesn't work super well

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,30 @@
-
-var wordList = ["racecar", "anna", "mom", "level", "kayak", "rotator", "stats", "wow", "rotor", "tenet", "sagas"];
+var wordList = [
+	"racecar",
+	"anna",
+	"mom",
+	"level",
+	"kayak",
+	"rotator",
+	"stats",
+	"wow",
+	"rotor",
+	"tenet",
+	"sagas",
+];
 var chosenWord = wordList[Math.round(Math.random() * 10)];
-var elope = function () {
-
-    console.log(wordList[0]); //?
-    document.getElementById("div01").innerHTML = wordList[0];
-    console.log(chosenWord);
-
-
-}
+var elope = function() {
+	console.log(wordList[0]); //?
+	document.getElementById("div01").innerHTML = wordList[0];
+	console.log(chosenWord);
+};
 
 elope();
- //array of palindromes
- //display to page "Press any Key to get started!"
- //array with alphabet?
- //counter for guesses left (variable decrement down from w/e number every run through the loop or switch)
- //div with list of letters already guessed (string just + to it if letters are not in current word)
- //div with '_ _ _ _ _ _ _' instead of a word, that upon correct guess replaces the '_' with letter. (reference fridge activity)
- //counter for wins (variable)
- //counter for losses (variable)
- //clear game after win/loss and restart with new word--increment win or loss (switch or if/else statement)---use code from calculator activity
+//array of palindromes
+//display to page "Press any Key to get started!"
+//array with alphabet?
+//counter for guesses left (variable decrement down from w/e number every run through the loop or switch)
+//div with list of letters already guessed (string just + to it if letters are not in current word)
+//div with '_ _ _ _ _ _ _' instead of a word, that upon correct guess replaces the '_' with letter. (reference fridge activity)
+//counter for wins (variable)
+//counter for losses (variable)
+//clear game after win/loss and restart with new word--increment win or loss (switch or if/else statement)---use code from calculator activity
