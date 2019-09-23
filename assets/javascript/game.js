32691e2e6a9747fca1b94b02d3aa5672d174337d
vudu <53414263+vuduland@users.noreply.github.com>
2019-09-22 22:49:56 -0500

    deleted bootstrap, went simple css, start game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,60 @@
-var wordList = [
-	"racecar",
-	"anna",
-	"mom",
-	"level",
-	"kayak",
-	"rotator",
-	"stats",
-	"wow",
-	"rotor",
-	"tenet",
-	"sagas",
-];
-var chosenWord = wordList[Math.round(Math.random() * 10)];
-var elope = function() {
-	console.log(wordList[0]); //?
-	document.getElementById("div01").innerHTML = wordList[0];
-	console.log(chosenWord);
-};
+var wordGuessGame = function() {
+  var wordList = [
+    "racecar",
+    "anna",
+    "mom",
+    "level",
+    "kayak",
+    "rotator",
+    "stats",
+    "wow",
+    "rotor",
+    "tenet",
+    "sagas"
+  ];
+  var chosenWord = wordList[Math.round(Math.random() * 10)];
+  var ask = " ";
+  var guessedLetters = [];
+  var guesses = 14;
 
-elope();
-//array of palindromes
-//display to page "Press any Key to get started!"
-//array with alphabet?
+  var elope = function() {
+    console.log("wordlist[0] = " + wordList[0]);
+    wordList[0] = document.getElementById("theWord").innerText;
+    console.log("wordlist[0] = " + wordList[0]);
+    console.log(chosenWord);
+  };
+	elope();
+	window.addEventListener("keyup", start, true);
+	var askUser = function (event) {
+		ask = event.key;
+    //ask = document.addEventListener("keyup",  ).char;
+    console.log(ask);
+
+    // switch () {
+    // 	case value:
+
+    // 		break;
+
+    // 	default:
+    // 		break;
+    // }
+
+    // guessedLetters[i] = document.addEventListener("keyup");
+  };
+  var start = function() {
+    //alert("Press any key to get started!");
+    askUser();
+  };
+
+  start();
+
+  var clearWindow = function() {};
+};
+wordGuessGame();
+
+//array of palindromes === wordList[]
+//display to page "Press any Key to get started!" === start()
+//empty array to add guesses to
 //counter for guesses left (variable decrement down from w/e number every run through the loop or switch)
 //div with list of letters already guessed (string just + to it if letters are not in current word)
 //div with '_ _ _ _ _ _ _' instead of a word, that upon correct guess replaces the '_' with letter. (reference fridge activity)
