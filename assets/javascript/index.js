a72ef6392d321ca095c7812b7b378d2658124a95
Victor <53414263+vuduland@users.noreply.github.com>
2020-01-21 16:33:09 -0600

    reorganized comments and labelled methods clearly

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,12 +1,15 @@
 const game = {
+
   wordList: ['racecar', 'anna', 'mom', 'level', 'kayak', 'rotator', 'stats', 'wow', 'rotor', 'tenet', 'sagas'],
-  randoWord: ' ',
-  guessesLeft: 9,
-  guessedLetters: [],
-  won: 'You won! Do you want to play again?',
-  lost: null,
   wins: 0,
   losses: 0,
+  won: 'You won! Do you want to play again?',
+  lost: '',
+  randoWord: [],
+  guessesLeft: 9,
+  guessedLetters: [],
+
+
   goodbye: 'Goodbye!',
 
 
@@ -19,13 +22,22 @@ const game = {
     lossCount: document.getElementById('loss-box')
   },
 
-  // **************************************************************************************************************************
-  // **************************************************************************************************************************
-  // methods:******************************************************************************************************************
-  // **************************************************************************************************************************
-  // **************************************************************************************************************************
+/*
+  **************************************************************************************************************************
+  methods:******************************************************************************************************************
+  **************************************************************************************************************************
+*/
+
+
+  initializeGame () {
+    console.log("OUTPUT: initializeGame -> initializeGame")
+    this.generateWord()
+    this.updateGameScreen()
+    console.log(`${game.randoWord} LOOK HERE `)
+  },
 
   win() {
+    console.log("OUTPUT: win -> win")
     this.wins++
     this.randoWord = null
     this.guessesLeft = 9
@@ -35,35 +47,28 @@ const game = {
   },
 
   lose() {
-
+      console.log("OUTPUT: lose -> lose")
       this.losses++
-      this.randoWord = null
+      console.log("OUTPUT: lose -> losses", this.losses)
+      this.randoWord = []
       this.guessesLeft = 9
       this.guessedLetters = []
       this.losePlayAgain()
       this.updateGameScreen()
   },
-
-  initializeGame () {
-
-
-    function generateWord()
-    {
+  generateWord(){
+    console.log("OUTPUT: generateWord -> generateWord")
     this.randoWord = this.wordList[Math.round(Math.random() * 10)]
     console.log('OUTPUT: generateWord -> this.randoWord', this.randoWord)
     return this.randoWord
-    }
-    generateWord()
-    this.updateGameScreen()
-    console.log(`${game.randoWord} LOOK HERE `)
   },
 
+
   updateGameScreen () {
+    console.log("OUTPUT: updateGameScreen -> updateGameScreen")
     this.elements.wordRandom.textContent = this.getHiddenWord()
-    // calls getHiddenWord function everytime the updateGameScreen property/function is ran.
-    // move this decrement to check if you win function
-    this.elements.remainCount.textContent = this.guessesLeft
-    this.elements.guessBox.textContent = this.guessedLetters
+    this.elements.remainCount.textContent = 9
+    this.elements.guessBox.textContent = []
     this.elements.winCount.textContent = this.wins
     this.elements.lossCount.textContent = this.losses
   },
@@ -133,15 +138,16 @@ const game = {
     }
     return false
   }
-  // **************************************************************************************************************************
-  // **************************************************************************************************************************
-  // </methods> ***************************************************************************************************************
-  // **************************************************************************************************************************
-  // **************************************************************************************************************************
+
+  /*
+  **************************************************************************************************************************
+  </methods> ***************************************************************************************************************
+  */
+
 }
 
 game.generateWord()
-game.check()
+// game.check()
 game.initializeGame()
 // game.updateGameScreen()
 // console.log(`${game.randoWord} LOOK HERE `)
@@ -150,71 +156,83 @@ document.onkeypress = function (event) {
   game.storeLetter(event.key)
 }
 
-/* a different method of sorting:
-***
-This method sorts the array, with function (a, b) {} as the argument. function (a, b) {}
-compares a and b's index values (i think) and then the wordList.sort() returns a random
-number between 0 and 1, then subtracts 0.5, so that if a is 0.4 it would return -0.1,
-and if b were 0.8, it would return 0.3, resulting in b being seen as greater value, and
-would be sorted before a in the array.
-***
 /*
-
-// **************************************************************************************************************************
-// comments: ****************************************************************************************************************
-// **************************************************************************************************************************
+**************************************************************************************************************************
+comments: ****************************************************************************************************************
 
 line 13:
   // references html elements and assigns them to key word.
 
 line 22:
-  // check () {
-  //     console.log(false)
-  //   if (document.getElementById('word-box') === this.randoWord) {
-  //     console.log(true)
-  //     return true
-  //   }
-  //   else {
-  //     return false
-  //   }
-  // },
+/*
+  check () {
+      console.log(false)
+    if (document.getElementById('word-box') === this.randoWord) {
+      console.log(true)
+      return true
+    }
+    else {
+      return false
+    }
+  },
+
 
 line 25:
-  // generateWord () {
-  //   this.randoWord = this.wordList[Math.round(Math.random() * 10)]
-  //   console.log('OUTPUT: generateWord -> this.randoWord', this.randoWord)
-  //   return this.randoWord
-  // },
+  generateWord () {
+    this.randoWord = this.wordList[Math.round(Math.random() * 10)]
+    console.log('OUTPUT: generateWord -> this.randoWord', this.randoWord)
+    return this.randoWord
+  },
 
 line 38:
-  // Generates random word from wordList, initalizes and stores word inside the randoWord property of the game object.
-  // This method references the game object with keyword 'this' and randoWord property with 'this.randoWord',
-  // then assigns wordList[] between 1 and 10
-  // Math.random() produces a number between 0 and 1, is multiplied by 10 and rounded to nearest integer.
+  Generates random word from wordList, initalizes and stores word inside the randoWord property of the game object.
+  This method references the game object with keyword 'this' and randoWord property with 'this.randoWord',
+  then assigns wordList[] between 1 and 10
+  Math.random() produces a number between 0 and 1, is multiplied by 10 and rounded to nearest integer.
 
 
 line 66:
-    // if (this.check()) {
-    //   this.win()
-    // } else if (this.check() === false) {
-    //   this.lose()
-    // }
+    if (this.check()) {
+      this.win()
+    } else if (this.check() === false) {
+      this.lose()
+    }
+
+line 67:
+    calls getHiddenWord function everytime the updateGameScreen property/function is ran.
+    move this decrement to check if you win function
+
+line 73:
+    Generates the current word with letters that haven't been guessed as underscores.
 
 line 74:
-    // updates DOM with current values of the game stats.
+    updates DOM with current values of the game stats.
 
 line 99:
-    // Accepts a letter input and stores its value to the game.guessedLetters property/var
-   // Returns whether or not letter is correct guess << think this needs removed <5:04pm 9.27.2019
+    Accepts a letter input and stores its value to the game.guessedLetters property/var
+    Returns whether or not letter is correct guess << think this needs removed <5:04pm 9.27.2019
 
 line 155:
-   // calling the generateWord() property/function to generate and return a random word from wordList[];
+   calling the generateWord() property/function to generate and return a random word from wordList[];
+
+  **************************************************************************************************************************
+  </comments> **************************************************************************************************************
+  **************************************************************************************************************************
+
+*/
 
 
+/*
 
-  // **************************************************************************************************************************
-  // </comments> **************************************************************************************************************
-  // **************************************************************************************************************************
+additional:
+
+a different method of sorting:
+
+This method sorts the array, with function (a, b) {} as the argument. function (a, b) {}
+compares a and b's index values (i think) and then the wordList.sort() returns a random
+number between 0 and 1, then subtracts 0.5, so that if a is 0.4 it would return -0.1,
+and if b were 0.8, it would return 0.3, resulting in b being seen as greater value, and
+would be sorted before a in the array.
 
 
 */
